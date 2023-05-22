cf8a9e65a165ba4119befc3e43586cbc438fdb83
prathyush1886 <74602287+prathyush1886@users.noreply.github.com>
2023-05-22 18:53:53 +0530

    Add files via upload

---
+++ Blink/Python-CAIDA/Blink-Master/python_code/util/logger.py
@@ -0,0 +1,21 @@
+import logging
+import logging.handlers
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+
+    # Remove the content of the log
+    open(log_file, 'w').close()
+
+    # Define the logger
+    main_logger = logging.getLogger(logger_name)
+
+    #formatter = logging.Formatter('%(asctime)s :: %(levelname)s | %(message)s')
+    if 'retr' in logger_name or 'instance' in logger_name:
+    	formatter = logging.Formatter('%(message)s')
+    else:
+    	formatter = logging.Formatter('%(levelname)s | %(message)s')
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=200000000000, backupCount=5)
+    handler.setFormatter(formatter)
+
+    main_logger.setLevel(level)
+    main_logger.addHandler(handler)
